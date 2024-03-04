58ef7507447583460b15aa8145104b054c3fdf55
HANSEONGHWA <ghk9206@hanmail.net>
2024-03-04 18:30:48 +0900

    초기세팅

---
+++ src/test/java/com/example/oneqjob2/OneQJob2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneqjob2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneQJob2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
